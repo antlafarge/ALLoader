97adaec38bb74888346e478dee4e138c18c9c33c
Antoine Lafarge <ant.lafarge@gmail.com>
2013-10-18 07:36:53 +0200

    ALM exporter improved

--- src/ALMLoader.js
+++ src/ALMLoader.js
@@ -2,26 +2,26 @@
  * @author ant-lafarge / http://ant.lafarge.free.fr/
  */
 
-THREE.ALMLoader = function ( showStatus ) {
+THREE.ALMLoader = function (showStatus) {
 
-	THREE.Loader.call( this, showStatus );
+	THREE.Loader.call(this, showStatus);
 
 };
 
-THREE.ALMLoader.prototype = Object.create( THREE.Loader.prototype );
+THREE.ALMLoader.prototype = Object.create(THREE.Loader.prototype);
 
-THREE.ALMLoader.prototype.load = function ( url, callback, texturePath ) {
+THREE.ALMLoader.prototype.load = function (url, callback, texturePath) {
 
 	var scope = this;
 
-	texturePath = texturePath ? texturePath : this.extractUrlBase( url );
+	texturePath = texturePath ? texturePath : this.extractUrlBase(url);
 
 	this.onLoadStart();
-	this.loadAjaxJSON( this, url, callback, texturePath );
+	this.loadAjaxJSON(this, url, callback, texturePath);
 
 };
 
-THREE.ALMLoader.prototype.loadAjaxJSON = function ( context, url, callback, texturePath, callbackProgress ) {
+THREE.ALMLoader.prototype.loadAjaxJSON = function (context, url, callback, texturePath, callbackProgress) {
 
 	var xhr = new XMLHttpRequest();
 
@@ -29,18 +29,18 @@ THREE.ALMLoader.prototype.loadAjaxJSON = function ( context, url, callback, text
 
 	xhr.onreadystatechange = function () {
 
-		if ( xhr.readyState === xhr.DONE ) {
+		if (xhr.readyState === xhr.DONE) {
 
-			if ( xhr.status === 200 || xhr.status === 0 ) {
+			if (xhr.status === 200 || xhr.status === 0) {
 
-				if ( xhr.responseText ) {
+				if (xhr.responseText) {
 				
-					var json = JSON.parse( xhr.responseText );
-					context.createModel( json, callback, texturePath );
+					var json = JSON.parse(xhr.responseText);
+					context.createModel(json, callback, texturePath);
 
 				} else {
 
-					console.warn( "THREE.ALMLoader: [" + url + "] seems to be unreachable or file there is empty" );
+					console.warn("THREE.ALMLoader: [" + url + "] seems to be unreachable or file there is empty");
 
 				}
 
@@ -52,56 +52,56 @@ THREE.ALMLoader.prototype.loadAjaxJSON = function ( context, url, callback, text
 
 			} else {
 
-				console.error( "THREE.ALMLoader: Couldn't load [" + url + "] [" + xhr.status + "]" );
+				console.error("THREE.ALMLoader: Couldn't load [" + url + "] [" + xhr.status + "]");
 
 			}
 
-		} else if ( xhr.readyState === xhr.LOADING ) {
+		} else if (xhr.readyState === xhr.LOADING) {
 
-			if ( callbackProgress ) {
+			if (callbackProgress) {
 
-				if ( length === 0 ) {
+				if (length === 0) {
 
-					length = xhr.getResponseHeader( "Content-Length" );
+					length = xhr.getResponseHeader("Content-Length");
 
 				}
 
-				callbackProgress( { total: length, loaded: xhr.responseText.length } );
+				callbackProgress({ total: length, loaded: xhr.responseText.length });
 
 			}
 
-		} else if ( xhr.readyState === xhr.HEADERS_RECEIVED ) {
+		} else if (xhr.readyState === xhr.HEADERS_RECEIVED) {
 
-			length = xhr.getResponseHeader( "Content-Length" );
+			length = xhr.getResponseHeader("Content-Length");
 
 		}
 
 	};
 
-	xhr.open( "GET", url, true );
-	xhr.send( null );
+	xhr.open("GET", url, true);
+	xhr.send(null);
 
 };
 
-THREE.ALMLoader.prototype.createModel = function ( json, callback, texturePath ) {
+THREE.ALMLoader.prototype.createModel = function (json, callback, texturePath) {
 
 	var materials = [];
-	for ( var i=0 ; i < json.materials.length ; i++ )
+	for (var i=0; i < json.materials.length; i++)
 	{
-		materials.push( parseMaterial( json.materials[i] ) );
+		materials.push(parseMaterial(json.materials[i]));
 	}
 
 	var meshes = [];
-	for ( var i=0 ; i < json.meshes.length ; i++ )
+	for (var i=0; i < json.meshes.length; i++)
 	{
-		meshes.push( parseMesh( json.meshes[i] ) );
+		meshes.push(parseMesh(json.meshes[i]));
 	}
 
-	function getMatIdFromName( name )
+	function getMatIdFromName(name)
 	{
-		for ( var i=0 ; i < materials.length ; i++ )
+		for (var i=0; i < materials.length; i++)
 		{
-			if ( materials[i].name == name )
+			if (materials[i].name == name)
 			{
 				return i;
 			}
@@ -109,108 +109,110 @@ THREE.ALMLoader.prototype.createModel = function ( json, callback, texturePath )
 		return -1;
 	}
 
-	function parseMaterial( jsonMat )
+	function parseMaterial(jsonMat)
 	{
 		// MATERIAL
 		var material = new THREE.MeshPhongMaterial();
 		material.name = jsonMat.name;
 		
 		var tex = jsonMat.texture;
-		if ( tex )
+		if (tex)
 		{
-			material.map = THREE.ImageUtils.loadTexture( texturePath + '/' + tex );
-			material.map.flipY = false;
+			material.map = THREE.ImageUtils.loadTexture(texturePath + '/' + tex);
+			//material.map.flipY = false;
 		}
 		
 		return material;
 	}
 
-	function parseMesh( jsonMesh )
+	function parseMesh(jsonMesh)
 	{
 		var geometry = new THREE.Geometry();
-		var material = null;
+		//var material = null;
 		
 		geometry.name = jsonMesh.name;
 		
-		var skinning = ( jsonMesh.skinIndices && jsonMesh.skinWeights && jsonMesh.skinIndices.length && jsonMesh.skinWeights.length );
+		var skinning = (jsonMesh.skinIndices && jsonMesh.skinWeights && jsonMesh.skinIndices.length && jsonMesh.skinWeights.length);
 		// There is only 1 material per mesh
-		var materialId = getMatIdFromName( jsonMesh.material );
-		if ( materialId === -1 )
+		var materialId = getMatIdFromName(jsonMesh.material);
+		if (materialId === -1)
 		{
 			material = new THREE.MeshNormalMaterial();
 		}
 		else
 		{
-			material = materials[ materialId ];
-			geometry.materials.push( material );
-			materialId = 0;
-			if ( skinning )
+			material = materials[materialId];
+			//geometry.materials.push(material);
+			//materialId = 0;
+			if (skinning)
 			{
 				material.skinning = true;
 			}
 		}
 		
 		// VERTICES
-		for ( var i=0 ; i < jsonMesh.vertices.length ; i+=3 )
+		for (var i=0; i < jsonMesh.vertices.length; i+=3)
 		{
-			geometry.vertices.push( new THREE.Vector3( jsonMesh.vertices[i], jsonMesh.vertices[i+1], jsonMesh.vertices[i+2] ) );
+			geometry.vertices.push(new THREE.Vector3(jsonMesh.vertices[i], jsonMesh.vertices[i+1], jsonMesh.vertices[i+2]));
 		}
 		
 		// NORMALS
 		geometry.normals = [];
-		for ( var i=0 ; i < jsonMesh.normals.length ; i+=3 )
+		for (var i=0; i < jsonMesh.normals.length; i+=3)
 		{
-			geometry.normals.push( new THREE.Vector3( jsonMesh.normals[i], jsonMesh.normals[i+1], jsonMesh.normals[i+2] ) );
+			geometry.normals.push(new THREE.Vector3(jsonMesh.normals[i], jsonMesh.normals[i+1], jsonMesh.normals[i+2]));
 		}
 		
 		// UVS
 		geometry.uvs = [];
-		for ( var i=0 ; i < jsonMesh.texcoord.length ; i+=2 )
+		for (var i=0; i < jsonMesh.texcoord.length; i+=2)
 		{
-			geometry.uvs.push( new THREE.UV( jsonMesh.texcoord[i], jsonMesh.texcoord[i+1] ) );
+			geometry.uvs.push(new THREE.Vector2(jsonMesh.texcoord[i], jsonMesh.texcoord[i+1]));
 		}
 		
 		// INDICES / FACES
 		var n = 0;
-		for ( var i=0 ; i < jsonMesh.indices.length ; i+=3 )
+		for (var i=0; i < jsonMesh.indices.length; i+=3)
 		{
-			geometry.faces.push( new THREE.Face3( jsonMesh.indices[i], jsonMesh.indices[i+1], jsonMesh.indices[i+2], geometry.normals[n], null, materialId ) );
-			if ( geometry.uvs.length )
+			geometry.faces.push(new THREE.Face3(jsonMesh.indices[i], jsonMesh.indices[i+1], jsonMesh.indices[i+2], geometry.normals[n], null, 0));
+			if (geometry.uvs.length)
 			{
-				geometry.faceVertexUvs[0].push( [ geometry.uvs[jsonMesh.indices[i]], geometry.uvs[jsonMesh.indices[i+1]], geometry.uvs[jsonMesh.indices[i+2]] ] );
+				geometry.faceVertexUvs[0].push([geometry.uvs[jsonMesh.indices[i]], geometry.uvs[jsonMesh.indices[i+1]], geometry.uvs[jsonMesh.indices[i+2]]]);
 			}
 			n++;
 		}
 		
-		if ( skinning )
+		if (skinning)
 		{
 			// BONES
-			//parseSkin( xmlMesh.querySelector( "Skin" ), geometry );
+			//parseSkin(xmlMesh.querySelector("Skin"), geometry);
 			
 			// SKININDEX && SKINWEIGHT
-			for ( var i=0 ; i < jsonMesh.skinIndices.length ; i+=4 )
+			for (var i=0; i < jsonMesh.skinIndices.length; i+=4)
 			{
-				var si0 = treatSkinIndex( jsonMesh.skinIndices[i  ] );
-				var si1 = treatSkinIndex( jsonMesh.skinIndices[i+1] );
-				//var si2 = treatSkinIndex( bi[i+2] );
-				//var si3 = treatSkinIndex( bi[i+3] );
-				//geometry.skinIndices.push( new THREE.Vector4( si0, si1, si2, si3 ) );
-				geometry.skinIndices.push( new THREE.Vector4( si0, si1, 0, 0 ) );
+				var si0 = treatSkinIndex(jsonMesh.skinIndices[i ]);
+				var si1 = treatSkinIndex(jsonMesh.skinIndices[i+1]);
+				//var si2 = treatSkinIndex(bi[i+2]);
+				//var si3 = treatSkinIndex(bi[i+3]);
+				//geometry.skinIndices.push(new THREE.Vector4(si0, si1, si2, si3));
+				geometry.skinIndices.push(new THREE.Vector4(si0, si1, 0, 0));
 
-				//geometry.skinWeights.push( new THREE.Vector4( bw[i], bw[i+1], bw[i+2], bw[i+3] ) );
-				geometry.skinWeights.push( new THREE.Vector4( jsonMesh.skinWeights[i], jsonMesh.skinWeights[i+1], 0, 0 ) );
+				//geometry.skinWeights.push(new THREE.Vector4(bw[i], bw[i+1], bw[i+2], bw[i+3]));
+				geometry.skinWeights.push(new THREE.Vector4(jsonMesh.skinWeights[i], jsonMesh.skinWeights[i+1], 0, 0));
 			}
 		}
 		
-		function treatSkinIndex( skinIndex )
+		function treatSkinIndex(skinIndex)
 		{
-			if ( skinIndex == -1 )
+			if (skinIndex == -1)
+			{
 				return 0;
+			}
 			return skinIndex;
 		}
 		
 		// Post-processing
-		if ( jsonMesh.normals.length === 0 )
+		if (jsonMesh.normals.length === 0)
 		{
 			geometry.computeFaceNormals();
 		}
@@ -218,54 +220,54 @@ THREE.ALMLoader.prototype.createModel = function ( json, callback, texturePath )
 		geometry.computeBoundingBox();
 		
 		var mesh;
-		if ( skinning )
+		if (skinning)
 		{
-			mesh = new THREE.SkinnedMesh( geometry, material );
+			mesh = new THREE.SkinnedMesh(geometry, material);
 		}
 		else
 		{
-			mesh = new THREE.Mesh( geometry, material );
+			mesh = new THREE.Mesh(geometry, material);
 		}
 		mesh.name = jsonMesh.name;
 
 		return mesh;
 	}
 	
-	function parseSkin( xmlSkin, geometry )
+	function parseSkin(xmlSkin, geometry)
 	{
-		if ( geometry.bones == null )
+		if (geometry.bones == null)
 			geometry.bones = [];
 
-		for ( var i=0 ; i < xmlBones.length ; i++ )
+		for (var i=0; i < xmlBones.length; i++)
 		{
 			var bone = {};
-			bone.name = xmlBones[i].getAttribute( "Name" );
+			bone.name = xmlBones[i].getAttribute("Name");
 			bone.parent = -1;
 
-			var im = xmlBones[i].getAttribute( "InitMatrix" ).split(' ');
-			bone.initMatrix = new THREE.Matrix4( im[0],im[3],im[6],im[9], im[1],im[4],im[7],im[10], im[2],im[5],im[8],im[11], 0,0,0,1 );
-			bone.invInitMatrix = new THREE.Matrix4().getInverse( bone.initMatrix );
+			var im = xmlBones[i].getAttribute("InitMatrix").split(' ');
+			bone.initMatrix = new THREE.Matrix4(im[0],im[3],im[6],im[9], im[1],im[4],im[7],im[10], im[2],im[5],im[8],im[11], 0,0,0,1);
+			bone.invInitMatrix = new THREE.Matrix4().getInverse(bone.initMatrix);
 
-			bone.pos = [ 0, 0, 0 ];
-			bone.rotq = [ 0, 0, 0, 1 ];
-			bone.scl = [ 1,1,1 ];
+			bone.pos = [0, 0, 0];
+			bone.rotq = [0, 0, 0, 1];
+			bone.scl = [1,1,1];
 
-			geometry.bones.push( bone );
+			geometry.bones.push(bone);
 		}
 	}
 	
 	var object3d = new THREE.Object3D();
-	if ( meshes.length == 1 )
+	if (meshes.length == 1)
 	{
 		object3d = meshes[0];
 	}
 	else
 	{
 		object3d = new THREE.Object3D();
-		for ( var i=0 ; i < meshes.length ; i++ )
+		for (var i=0; i < meshes.length; i++)
 		{
-			object3d.add( meshes[i] );
+			object3d.add(meshes[i]);
 		}
 	}
-	callback( object3d );
+	callback(object3d);
 };
