6eab073e3c018a2e2a67bddc9339e222c90d05ae
Antoine Lafarge <ant.lafarge@gmail.com>
2024-09-02 05:31:17 +0200

    Rework json file

--- loaders/ALLoader.js
+++ loaders/ALLoader.js
@@ -60,10 +60,10 @@ export class ALLoader extends THREE.Loader
 			for (const matName in json.materials)
 			{
 				const jsonMat = json.materials[matName];
-				if (jsonMat.multi && jsonMat.multi.length)
+				if (jsonMat.ml && jsonMat.ml.length)
 				{
 					const materials2 = materials[matName];
-					for (const matName2 of jsonMat.multi)
+					for (const matName2 of jsonMat.ml)
 					{
 						materials2.push(materials[matName2]);
 					}
@@ -101,10 +101,9 @@ export class ALLoader extends THREE.Loader
 			for (const animName in json.animations)
 			{
 				const jsonAnimation = json.animations[animName];
-				const skeleton = skeletons[jsonAnimation.skeleton];
-				const animation = this.parseAnimation(jsonAnimation, skeleton);
+				const animation = this.parseAnimation(jsonAnimation, animName);
 				animation.name = animName;
-				const mesh = meshes[jsonAnimation.mesh];
+				const mesh = meshes[jsonAnimation.ms];
 				mesh.animations.push(animation);
 				animations[animName] = animation;
 			}
@@ -119,45 +118,41 @@ export class ALLoader extends THREE.Loader
 
 		let material;
 
-		if (jsonMat.multi)
+		if (jsonMat.ml)
 		{
-			// MULTI-MATERIAL
+			// MULTI-MATERIALS
 			material = [];
 		}
 		else
 		{
 			material = new THREE.MeshPhongMaterial();
 
-			// DIFFUSE
-			if (jsonMat.diffuse)
+			// COLOR
+			if (jsonMat.cl)
 			{
-				material.color.r = jsonMat.diffuse[0] / 255;
-				material.color.g = jsonMat.diffuse[1] / 255;
-				material.color.b = jsonMat.diffuse[2] / 255;
+				material.color.set(jsonMat.cl[0] / 255, jsonMat.cl[1] / 255, jsonMat.cl[2] / 255);
 			}
 			
 			// SPECULAR
-			if (jsonMat.specular)
+			if (jsonMat.sp)
 			{
-				material.specular.r = jsonMat.specular[0] / 255;
-				material.specular.g = jsonMat.specular[1] / 255;
-				material.specular.b = jsonMat.specular[2] / 255;
+				material.specular.set(jsonMat.sp[0] / 255, jsonMat.sp[1] / 255, jsonMat.sp[2] / 255);
 			}
 
 			// OPACITY
-			if (jsonMat.opacity)
+			if (jsonMat.op)
 			{
-				material.opacity = jsonMat.opacity;
-				if (jsonMat.opacity != 1)
+				material.opacity = jsonMat.op;
+				if (material.opacity != 1)
 				{
 					material.transparent = true;
 				}
 			}
 			
-			// TEXTURE
-			if (jsonMat.texture)
+			// COLOR MAP (TEXTURE)
+			if (jsonMat.cm)
 			{
-				const textureUrl = `${texturePath}/${jsonMat.texture}`;
+				const textureUrl = `${texturePath}/${jsonMat.cm}`;
 
 				const onTextureLoaded = (texture) =>
 				{
@@ -171,13 +166,13 @@ export class ALLoader extends THREE.Loader
 				material.map = (new THREE.TextureLoader()).load(textureUrl, onTextureLoaded, onTextureProgress, onTextureError);
 			}
 			
-			if (jsonMat.side)
+			if (jsonMat.sd)
 			{
-				if (jsonMat.side === "double")
+				if (jsonMat.sd == "double")
 				{
 					material.side = THREE.DoubleSide;
 				}
-				else if (jsonMat.side === "back" || jsonMat.side === "reverse")
+				else if (jsonMat.sd == "back" || jsonMat.sd == "reverse")
 				{
 					material.side = THREE.BackSide;
 				}
@@ -197,11 +192,11 @@ export class ALLoader extends THREE.Loader
 		for (const jsonBone of jsonSkeleton)
 		{
 			const bone = new THREE.Bone();
-			bone.name = jsonBone.name;
-			bone.userData.parent = jsonBone.parent;
-			bone.position.fromArray(jsonBone.pos);
-			bone.quaternion.fromArray(jsonBone.rotq);
-			bone.scale.fromArray(jsonBone.scl);
+			bone.name = jsonBone.nm;
+			bone.userData.parent = jsonBone.pr;
+			bone.position.fromArray(jsonBone.ps);
+			bone.quaternion.fromArray(jsonBone.rt);
+			bone.scale.fromArray(jsonBone.sc);
 			bones.push(bone);
 		}
 
@@ -222,7 +217,7 @@ export class ALLoader extends THREE.Loader
 		console.debug("parseMesh", jsonMesh, materials, skeletons);
 
 		// MATERIAL
-		const material = materials[jsonMesh.material];
+		const material = materials[jsonMesh.mt];
 		if (material == null)
 		{
 			material = new THREE.MeshNormalMaterial();
@@ -231,23 +226,23 @@ export class ALLoader extends THREE.Loader
 		// GEOMETRY
 		const geometry = new THREE.BufferGeometry();
 
-		// VERTICES
-		if (jsonMesh.vertex_indices && jsonMesh.vertex_indices.length)
+		// VERTICES (Vertex Positions and Vertex Indices)
+		if (jsonMesh.vi && jsonMesh.vi.length)
 		{
 			// INDEXED (remove indices)
-			const verticesSize = 3 * jsonMesh.vertex_indices.reduce((acc, value) => (acc + value.length), 0);
+			const verticesSize = 3 * jsonMesh.vi.reduce((acc, value) => (acc + value.length), 0);
 			const vertices = new Float32Array(verticesSize);
 			let index = 0;
 			let materialIndex = 0;
-			for (const groupVertexIndices of jsonMesh.vertex_indices)
+			for (const groupVertexIndices of jsonMesh.vi)
 			{
 				geometry.addGroup((index / 3), groupVertexIndices.length, materialIndex++);
 				for (const vertexIndex of groupVertexIndices)
 				{
 					const vertexIndexPosition = vertexIndex * 3;
-					vertices[index++] = jsonMesh.vertex_positions[vertexIndexPosition];
-					vertices[index++] = jsonMesh.vertex_positions[vertexIndexPosition + 1];
-					vertices[index++] = jsonMesh.vertex_positions[vertexIndexPosition + 2];
+					vertices[index++] = jsonMesh.vp[vertexIndexPosition];
+					vertices[index++] = jsonMesh.vp[vertexIndexPosition + 1];
+					vertices[index++] = jsonMesh.vp[vertexIndexPosition + 2];
 				}
 			}
 			geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
@@ -255,17 +250,17 @@ export class ALLoader extends THREE.Loader
 		else
 		{
 			// NOT INDEXED
-			geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(jsonMesh.vertex_positions), 3));
+			geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(jsonMesh.vp), 3));
 		}
 
-		// NORMALS
-		if (jsonMesh.face_normals && jsonMesh.face_normals.length && jsonMesh.vertex_indices && jsonMesh.vertex_indices.length)
+		// NORMALS (Face normals)
+		if (jsonMesh.fn && jsonMesh.fn.length && jsonMesh.vi && jsonMesh.vi.length)
 		{
 			// INDEXED (remove indices)
-			const normalsSize = 3 * jsonMesh.face_normals.reduce((acc, value) => (acc + value.length), 0);
+			const normalsSize = 3 * jsonMesh.fn.reduce((acc, value) => (acc + value.length), 0);
 			const normals = new Float32Array(normalsSize);
 			let index = 0;
-			for (const groupFaceNormals of jsonMesh.face_normals)
+			for (const groupFaceNormals of jsonMesh.fn)
 			{
 				for (let i = 0; i < groupFaceNormals.length; i += 3)
 				{
@@ -282,10 +277,10 @@ export class ALLoader extends THREE.Loader
 			}
 			geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
 		}
-		else if (jsonMesh.normals)
+		else if (jsonMesh.nm)
 		{
 			// NOT INDEXED
-			geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(jsonMesh.normals), 3));
+			geometry.setAttribute('normal', new THREE.BufferAttribute(new Float32Array(jsonMesh.nm), 3));
 		}
 		else
 		{
@@ -294,46 +289,46 @@ export class ALLoader extends THREE.Loader
 		}
 
 		// UVS
-		if (jsonMesh.uv_indices && jsonMesh.uv_indices.length)
+		if (jsonMesh.ui && jsonMesh.ui.length)
 		{
 			// INDEXED (remove indices)
-			const uvs = new Float32Array(jsonMesh.uv_indices.length * 2);
-			for (let i = 0, sz = jsonMesh.uv_indices.length; i < sz; i++)
+			const uvs = new Float32Array(jsonMesh.ui.length * 2);
+			for (let i = 0, sz = jsonMesh.ui.length; i < sz; i++)
 			{
-				const index = jsonMesh.uv_indices[i];
+				const index = jsonMesh.ui[i];
 				uvs[2 * i] = jsonMesh.uv[2 * index];
 				uvs[2 * i + 1] = jsonMesh.uv[2 * index + 1];
 			}
 			geometry.setAttribute('uv', new THREE.BufferAttribute(uvs, 2));
 		}
-		else if (jsonMesh.uvs)
+		else if (jsonMesh.uv)
 		{
 			// NOT INDEXED
-			geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(jsonMesh.uvs), 2));
+			geometry.setAttribute('uv', new THREE.BufferAttribute(new Float32Array(jsonMesh.uv), 2));
 		}
 
 		// MESH
 		let mesh;
-		if (jsonMesh.skin_indices && jsonMesh.skin_weights)
+		if (jsonMesh.si && jsonMesh.sw)
 		{
-			const skinIndicesSize = 4 * jsonMesh.vertex_indices.reduce((acc, value) => (acc + value.length), 0);
+			const skinIndicesSize = 4 * jsonMesh.vi.reduce((acc, value) => (acc + value.length), 0);
 			const skinIndices = new Uint16Array(skinIndicesSize);
 			const skinWeights = new Float32Array(skinIndicesSize);
 			let index = 0;
-			for (const groupVertexIndices of jsonMesh.vertex_indices)
+			for (const groupVertexIndices of jsonMesh.vi)
 			{
 				for (const vertexIndex of groupVertexIndices)
 				{
 					const skinIndexPosition = vertexIndex * 4;
-					const i0 = jsonMesh.skin_indices[skinIndexPosition];
-					const i1 = jsonMesh.skin_indices[skinIndexPosition+1];
-					const i2 = jsonMesh.skin_indices[skinIndexPosition+2];
-					const i3 = jsonMesh.skin_indices[skinIndexPosition+3];
+					const i0 = jsonMesh.si[skinIndexPosition];
+					const i1 = jsonMesh.si[skinIndexPosition+1];
+					const i2 = jsonMesh.si[skinIndexPosition+2];
+					const i3 = jsonMesh.si[skinIndexPosition+3];
 
-					const w0 = jsonMesh.skin_weights[skinIndexPosition];
-					const w1 = jsonMesh.skin_weights[skinIndexPosition+1];
-					const w2 = jsonMesh.skin_weights[skinIndexPosition+2];
-					const w3 = jsonMesh.skin_weights[skinIndexPosition+3];
+					const w0 = jsonMesh.sw[skinIndexPosition];
+					const w1 = jsonMesh.sw[skinIndexPosition+1];
+					const w2 = jsonMesh.sw[skinIndexPosition+2];
+					const w3 = jsonMesh.sw[skinIndexPosition+3];
 
 					const total = w0 + w1 + w2 + w3;
 					if (total < 0.1)
@@ -382,9 +377,9 @@ export class ALLoader extends THREE.Loader
 			mesh.userData.animated = true;
 			
 			// SKELETON
-			if (jsonMesh.skeleton && skeletons)
+			if (jsonMesh.sk && skeletons)
 			{
-				const skeleton = skeletons[jsonMesh.skeleton];
+				const skeleton = skeletons[jsonMesh.sk];
 				if (skeleton)
 				{
 					for (const bone of skeleton.bones)
@@ -396,7 +391,7 @@ export class ALLoader extends THREE.Loader
 					}
 
 					mesh.bind(skeleton);
-					mesh.userData.skeletonName = jsonMesh.skeleton;
+					mesh.userData.skeletonName = jsonMesh.sk;
 				}
 			}
 		}
@@ -405,49 +400,51 @@ export class ALLoader extends THREE.Loader
 			mesh = new THREE.Mesh(geometry, material);
 		}
 
-		if (jsonMesh.position)
+		if (jsonMesh.ps)
 		{
-			mesh.position.fromArray(jsonMesh.position);
+			mesh.position.fromArray(jsonMesh.ps);
 		}
 
-		if (jsonMesh.rotation)
+		if (jsonMesh.rt)
 		{
-			mesh.quaternion.fromArray(jsonMesh.rotation);
+			mesh.quaternion.fromArray(jsonMesh.rt);
 		}
 
-		if (jsonMesh.scale)
+		if (jsonMesh.sc)
 		{
-			mesh.scale.fromArray(jsonMesh.scale);
+			mesh.scale.fromArray(jsonMesh.sc);
 		}
 
 		return mesh;
 	}
 
-	parseAnimation(jsonAnim, skeleton)
+	parseAnimation(jsonAnim, animName)
 	{
+		console.debug("parseAnimation", jsonAnim, animName);
+
 		const tracks = [];
 
-		for (let jsonTrackIndex = 0; jsonTrackIndex < jsonAnim.hierarchy.length; jsonTrackIndex++)
+		for (const jsonTrack of jsonAnim.hr)
 		{
-			const jsonTrack = jsonAnim.hierarchy[jsonTrackIndex];
-			const boneName = skeleton.bones[jsonTrackIndex].name;
+			const boneName = jsonTrack.bn;
 			const times = [];
 			const positions = [];
 			const orientations = [];
 			const scales = [];
-			for (const jsonFrame of jsonTrack.keys)
+
+			for (const jsonFrame of jsonTrack.ky)
 			{
-				times.push(jsonFrame.time);
-				positions.push(...jsonFrame.pos);
-				orientations.push(...jsonFrame.rot);
-				scales.push(...jsonFrame.scl);
+				times.push(jsonFrame.tm);
+				positions.push(...jsonFrame.ps);
+				orientations.push(...jsonFrame.rt);
+				scales.push(...jsonFrame.sc);
 			}
 			tracks.push(new THREE.VectorKeyframeTrack(`${boneName}.position`, times, positions));
 			tracks.push(new THREE.QuaternionKeyframeTrack(`${boneName}.quaternion`, times, orientations));
 			tracks.push(new THREE.VectorKeyframeTrack(`${boneName}.scale`, times, scales));
 		}
 
-		return new THREE.AnimationClip(jsonAnim.name, jsonAnim.length, tracks);
+		return new THREE.AnimationClip(animName, jsonAnim.dr, tracks);
 	}
 	
 	static cloneAnimationData(data, newName)
